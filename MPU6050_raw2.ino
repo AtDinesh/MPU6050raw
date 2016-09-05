3b5a10c0fea1b438f81b8ae2cacbcb876e2e0b14
Dinesh Atchuthan <datchuth@laas.fr>
2016-09-05 22:44:19 +0200

    remove SPI + flash

--- MPU6050_raw2.ino
+++ MPU6050_raw2.ino
@@ -1,44 +1,12 @@
 
 #include <HardWire.h>
-#include <SPI.h>
 
 //Pin MAP
-#define LED_PIN PA14    
-#define POWER_EN_PIN PB1
-#define MEM_HOLD_PIN PA3
-#define MEM_CS_PIN PA4
-#define ADC_PHOT_PIN PA2
-
-
-//COMMANDS FLASH
-#define W_EN   0x06  //write enable
-#define W_DE  0x04  //write disable
-#define R_SR1 0x05  //read status reg 1
-#define R_SR2 0x35  //read status reg 2
-#define W_SR  0x01  //write status reg
-#define PAGE_PGM  0x02  //page program
-#define QPAGE_PGM 0x32  //quad input page program
-#define BLK_E_64K 0xD8  //block erase 64KB
-#define BLK_E_32K 0x52  //block erase 32KB
-#define SECTOR_E  0x20  //sector erase 4KB
-#define CHIP_ERASE  0xc7  //chip erase
-#define CHIP_ERASE2 0x60  //=CHIP_ERASE
-#define E_SUSPEND 0x75  //erase suspend
-#define E_RESUME  0x7a  //erase resume
-#define PDWN    0xb9  //power down
-#define HIGH_PERF_M 0xa3  //high performance mode
-#define CONT_R_RST  0xff  //continuous read mode reset
-#define RELEASE   0xab  //release power down or HPM/Dev ID (deprecated)
-#define R_MANUF_ID  0x90  //read Manufacturer and Dev ID (deprecated)
-#define R_UNIQUE_ID 0x4b  //read unique ID (suggested)
-#define R_JEDEC_ID  0x9f  //read JEDEC ID = Manuf+ID (suggested)
-#define READ    0x03
-#define FAST_READ 0x0b
-
+#define LED_PIN PB1    
 
 
 HardWire HWire(1, I2C_FAST_MODE); // I2c1 I2C_FAST_MODE
-const int MPU_addr=0x69;//0x68;  // I2C address of the MPU-6050
+const int MPU_addr=0x68;//0x69;  // I2C address of the MPU-6050
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 int loop_time=0;
 int t_tmp = 0;
@@ -46,21 +14,7 @@ int i;
 int dt = 1000; //(1ms)
 int next_time=0;
 
-//#define LED PB1
-//Kmeans
-float H_mean = 1024;
-float L_mean = 1024;
-
 void setup() {
-  // Setup SPI 1
-  SPI.begin(); //Initialize the SPI_1 port.
-  SPI.setBitOrder(MSBFIRST); // Set the SPI_1 bit order
-  SPI.setDataMode(SPI_MODE0); //Set the  SPI_2 data mode 0
-  SPI.setClockDivider(SPI_CLOCK_DIV16);      // Slow speed (72 / 16 = 4.5 MHz SPI_1 speed)
-  SPI.setClockDivider(SPI_CLOCK_DIV128);    
-  pinMode(MEM_CS_PIN, OUTPUT);
-  pinMode(MEM_HOLD_PIN, OUTPUT);
-  digitalWrite(MEM_HOLD_PIN, LOW); 
   
   // initialize digital pin PA14 as an output.
   pinMode(LED_PIN, OUTPUT);
@@ -77,8 +31,6 @@ void setup() {
   HWire.endTransmission();
   digitalWrite(LED_PIN, LOW);
 
-  //read memory inique ID
-  //print_ID();
   delay(1000);
 }
 
@@ -92,9 +44,6 @@ void loop() {
       unsigned char c = Serial.read();
       switch(c)
       {
-        case 'i':
-          print_ID();
-          break;
         case 'h':
           print_help();
           break;
@@ -109,10 +58,9 @@ void loop() {
  
   //blink RED LED
   digitalWrite(LED_PIN, (millis()%1000)<10);
-  //Serial.println("mes");
+
   MPU_read_RAW();
-  //write_flash();
-  //print_ID();
+
   if (flag_send_raw)
   {
     Serial.print(AcX);
@@ -126,27 +74,6 @@ void loop() {
   }
 
   while(micros()<next_time) delayMicroseconds(1); //1khz
-
-  //digitalWrite(LED, LOW);
-  next_time+=dt;
-
-  int photo = analogRead(PA2);
-  int e = photo;
-
-  if (e >(H_mean+L_mean)/2 )
-  {
-    digitalWrite(PA3, HIGH);
-    H_mean=H_mean*0.9+e*0.1;
-  }
-  else
-  {
-    L_mean=L_mean*0.9+e*0.1;
-    digitalWrite(PA3, LOW);
-  }
-  //Serial.print("H_mean = "); 
-  //Serial.print(H_mean);
-  //Serial.print("\tL_mean = "); 
-  //Serial.println(L_mean);
   }
 }
 
@@ -180,58 +107,12 @@ char MPU_read_RAW()
   GyZ=HWire.read()<<8|HWire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 }
 
-void write_flash()
-{
-  digitalWrite(MEM_CS_PIN, LOW); // manually take CSN low for SPI_1 transmission
-  SPI.transfer(0x55); //Send the HEX data 0x55 over SPI-1 port and store the received byte to the <data> variable.
-  SPI.transfer(0x55); //Send the HEX data 0x55 over SPI-1 port and store the received byte to the <data> variable.
-  SPI.transfer(0x55); //Send the HEX data 0x55 over SPI-1 port and store the received byte to the <data> variable.
-  SPI.transfer(0x55); //Send the HEX data 0x55 over SPI-1 port and store the received byte to the <data> variable.
-  SPI.transfer(0x55); //Send the HEX data 0x55 over SPI-1 port and store the received byte to the <data> variable.
-  SPI.transfer(0x55); //Send the HEX data 0x55 over SPI-1 port and store the received byte to the <data> variable.
-
-  SPI.transfer(0x55); //Send the HEX data 0x55 over SPI-1 port and store the received byte to the <data> variable.
-  SPI.transfer(0x55); //Send the HEX data 0x55 over SPI-1 port and store the received byte to the <data> variable.
-  SPI.transfer(0x55); //Send the HEX data 0x55 over SPI-1 port and store the received byte to the <data> variable.
-  SPI.transfer(0x55); //Send the HEX data 0x55 over SPI-1 port and store the received byte to the <data> variable.
-  SPI.transfer(0x55); //Send the HEX data 0x55 over SPI-1 port and store the received byte to the <data> variable.
-  SPI.transfer(0x55); //Send the HEX data 0x55 over SPI-1 port and store the received byte to the <data> variable.
-  
-  digitalWrite(MEM_CS_PIN, HIGH); // manually take CSN high between spi transmissions
-}
-
-
-void print_ID()
-{
-  uint64_t uid;
-  uint8_t *arr;
-  arr = (uint8_t*)&uid;
-  digitalWrite(MEM_CS_PIN, LOW); // manually take CSN low for SPI_1 transmission
-  SPI.transfer(R_UNIQUE_ID);
-  SPI.transfer(0x00);
-  SPI.transfer(0x00);
-  SPI.transfer(0x00);
-  SPI.transfer(0x00);
-  //for little endian machine only
-  //Serial.println("Memory ID is:");
-  for(int i=7;i>=0;i--)
-  {
-    arr[i] = SPI.transfer(0x00);
-    if (arr[i]<0x10)Serial.print('0');
-    Serial.print(arr[i],HEX);
-    Serial.print(" ");
-  }
-  Serial.print('\n');
-  digitalWrite(MEM_CS_PIN, HIGH); // manually take CSN high between spi transmissions
-}
 
 
 void print_help()
 {
- Serial.println("IMU logger - Thomas FLAYOLS LAAS CNRS");
  Serial.println("Commands are");
  Serial.println(" h: Help ");
- Serial.println(" i: return Unique ID");
  Serial.println(" r: send RAW data at 1Khz (Ax,Ay,Az,Temp,Gx,Gy,Gz)");
  Serial.println(" R: stop sending raw data");
 }
